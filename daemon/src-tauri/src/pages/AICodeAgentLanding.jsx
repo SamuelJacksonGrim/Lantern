ea3b89dd8c537448b3c29eae432361fe084e032b
Samuel Jackson Grim <samgrim97@gmail.com>
2025-11-30 14:04:01 -0700

    Update AICodeAgentLanding.jsx

--- daemon/src-tauri/src/pages/AICodeAgentLanding.jsx
+++ daemon/src-tauri/src/pages/AICodeAgentLanding.jsx
@@ -1,2 +1,3 @@
 import InfoCard from '../components/InfoCard';
 import DemoTerminal from '../components/DemoTerminal';
+// import LanternFooter from '../components/LanternFooter'; // if you split footer
